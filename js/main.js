6a1c7c727168a32194f6b7cf040bf9389459306a
Messef <85040583+AkTewari123@users.noreply.github.com>
2025-03-11 00:30:03 -0400

    made starting animatino - needs to be refined

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import hideMidoriOnload from "/js/functions/midorionload.js";
+import drawHorizontalLine from "/js/functions/drawhorizontalline.js";
+const main = () => {
+  drawHorizontalLine();
+  hideMidoriOnload();
+};
+main();
