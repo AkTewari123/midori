32d1b71f1e728ed59495733ddbe4fd45363fba0a
Messef <85040583+AkTewari123@users.noreply.github.com>
2025-03-31 18:41:32 -0400

    pushing code to main to view vercel deployment

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Skip animation flag - set to true to bypass intro animations for development
 // Transitioning prop is for when you go to index.html from another page in the website
 // const skipAnimation = localStorage.getItem("pageTransition") === "home";
-const skipAnimation = sessionStorage.getItem("payload") || true;
+const skipAnimation = true;
 
 const enableParallax = () => {
   const blockOne = document.getElementById("block-one");
