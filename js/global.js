3f73398baf62b8be0cac1bb9f662c00dc3871a41
Messef <85040583+AkTewari123@users.noreply.github.com>
2025-03-31 09:14:17 -0400

    added code reusability for the navbar and the sources trigger

--- js/global.js
+++ js/global.js
@@ -1,6 +1,43 @@
 const container = document.getElementById("sources-container");
 const overlay = document.getElementById("white-nav-overlay");
+const menuGreenTransition = () => {
+  document
+    .getElementById("menu-link-nav")
+    .addEventListener("mouseenter", (event) => {
+      const greenDot = document.getElementById("greendot");
+      const menuElement = document.getElementById("menu-link-nav");
 
+      // Get the mouse X position relative to the "menu" element
+      const menuRect = menuElement.getBoundingClientRect();
+      const relativeX = event.clientX - menuRect.left; // Adjust X coordinate
+      const relativeY = event.clientY - menuRect.top; // Adjust X coordinate
+      console.log(menuRect);
+      // Position the green dot horizontally within "menu"
+      greenDot.style.left = `${relativeX}px`;
+      greenDot.style.top = `${relativeY}px`;
+      // Scale it by 20
+      greenDot.style.transform = "scale(20)";
+
+      console.log(greenDot);
+    });
+  document
+    .getElementById("menu-link-nav")
+    .addEventListener("mouseleave", (event) => {
+      const greenDot = document.getElementById("greendot");
+      const menuElement = document.getElementById("menu-link-nav");
+
+      const menuRect = menuElement.getBoundingClientRect(); // get pos of mouse relative to menu box
+      const relativeX = event.clientX - menuRect.left;
+      const relativeY = event.clientY - menuRect.top;
+      console.log(menuRect);
+      greenDot.style.left = `${relativeX}px`;
+      greenDot.style.top = `${relativeY}px`;
+
+      greenDot.style.transform = "scale(.01)";
+
+      console.log(greenDot);
+    });
+};
 const main = () => {
   document
     .getElementById("sources-trigger-open")
@@ -31,6 +68,7 @@ const main = () => {
   document.getElementById("white-nav-close").onclick = () => {
     overlay.style.clipPath = "inset(0 0 100% 0)"; // Expand from top
   };
+  menuGreenTransition();
 };
 
 main();
